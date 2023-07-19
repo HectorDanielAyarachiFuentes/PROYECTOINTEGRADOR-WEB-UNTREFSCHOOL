09378d6013491583f693680d6950d35fb1c78d9c
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-07-19 13:37:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
 //Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
+function open_close_menu() {
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}
 
 //Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
 
-if (window.innerWidth < 760){
+if (window.innerWidth < 760) {
 
     body.classList.add("body_move");
     side_menu.classList.add("menu__side_move");
@@ -22,15 +22,15 @@ if (window.innerWidth < 760){
 
 //Haciendo el menú responsive(adaptable)
 
-window.addEventListener("resize", function(){
+window.addEventListener("resize", function () {
 
-    if (window.innerWidth > 760){
+    if (window.innerWidth > 760) {
 
         body.classList.remove("body_move");
         side_menu.classList.remove("menu__side_move");
     }
 
-    if (window.innerWidth < 760){
+    if (window.innerWidth < 760) {
 
         body.classList.add("body_move");
         side_menu.classList.add("menu__side_move");
@@ -39,25 +39,25 @@ window.addEventListener("resize", function(){
 });
 
 
-window.onscroll = function() { // Cuando se realiza el scroll
+window.onscroll = function () { // Cuando se realiza el scroll
     var scrollToTopButton = document.getElementById("scroll-to-top");
-    
-    if (window.pageYOffset > 0) { // Si se ha desplazado hacia abajo
-      scrollToTopButton.style.display = "block"; // Mostrar la flecha
-    } else { // Si está en la parte superior de la página
-      scrollToTopButton.style.display = "none"; // Ocultar la flecha
-    }
-  };
-  
-  function scrollToTop() { // Función para desplazarse hacia arriba al hacer clic en la flecha
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-  
 
-  function mostrarSeccion(id) {
+    if (window.pageYOffset > 0) { // Si se ha desplazado hacia abajo
+        scrollToTopButton.style.display = "block"; // Mostrar la flecha
+    } else { // Si está en la parte superior de la página
+        scrollToTopButton.style.display = "none"; // Ocultar la flecha
+    }
+};
+
+function scrollToTop() { // Función para desplazarse hacia arriba al hacer clic en la flecha
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
+function mostrarSeccion(id) {
     var secciones = document.getElementsByClassName("section");
     for (var i = 0; i < secciones.length; i++) {
         if (secciones[i].id === id) {
